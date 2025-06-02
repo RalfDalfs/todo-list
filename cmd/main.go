f4cf12d82871c65c66a9d5cea5c2f3f4fa8bd990
Ralf Dalf <maxim.tzrilin@yandex.ru>
2025-06-02 21:52:38 +0300

    Add db create

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"github.com/RalfDalfs/todo-list/internal/db"
 	"github.com/RalfDalfs/todo-list/internal/server"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 func main() {
+	godotenv.Load()
+
+	dbFile := os.Getenv("TODO_DBFILE")
+	if dbFile == "" {
+		dbFile = "./scheduler.db"
+	}
+
+	err := db.Init(dbFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	server.Run()
+
 }
---
+++ internal/db/db.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	_ "modernc.org/sqlite"
+	"os"
+)
+
+var db *sql.DB
+
+const schema = `CREATE TABLE IF NOT EXISTS scheduler (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    date CHAR(8) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    comment TEXT NOT NULL,
+    repeat VARCHAR(255)
+);
+
+
+CREATE INDEX IF NOT EXISTS scheduler_date ON scheduler(date);
+`
+
+func Init(dbFile string) error {
+	_, err := os.Stat(dbFile)
+	dbExists := !os.IsNotExist(err)
+	db, err = sql.Open("sqlite", dbFile)
+	defer db.Close()
+	if err != nil {
+		return fmt.Errorf("Не удалось открыть БД:%v", err)
+	}
+	if !dbExists {
+		fmt.Println("База данных не найдена, создается новая")
+		_, err = db.Exec(schema)
+		if err != nil {
+			return fmt.Errorf("Ошибка при создании таблицы: %v", err)
+		}
+	}
+	return nil
+}
